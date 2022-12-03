d3e8eae4ae01f7ef030e115b3cbfddc8b8b6f713
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2022-12-03 23:21:33 +0300

    feat: реализовать логику игры и проверить ее работу

---
+++ scripts/index.js
@@ -0,0 +1,51 @@
+// NB! Проблемы
+// 1) Если пользователь ничего не вводит, то считается, что он выводит 0 и выходит "MISS" => добавить условие
+// 2) Пользователь не может закрыть модальное окно, пока не закончится цикл
+// 3) Можно ввести строковое значение и оно будет считаться как "MISS" => валидация
+// 4) Можно стрелять в одну и ту же клетку (таким образом потопить корабль) => запретить это действие
+
+// ПЕРЕМЕННЫЕ
+// Позиция каждой клетки корабля
+const location1 = 3;
+const location2 = 4;
+const location3 = 5;
+
+// Номер ячейки для выстрела
+let guess;
+// Количество попаданий
+let hits = 0;
+// Количество попыток
+let guesses = 0;
+
+// Потоплен корабль или нет
+let isSunk = true;
+
+
+// ФУНКЦИИ
+while (isSunk) {
+  guess = prompt('Ready, aim, fire! (enter a number 0-6):');
+
+  if (+guess < 0 || +guess > 6) {
+    alert('Please enter a valid cell number!');
+  } else {
+    guesses += 1;
+
+    if (+guess === location1 || +guess === location2 || +guess === location3) {
+      alert('HIT!');
+      hits += 1;
+
+      if (hits === 3) {
+        isSunk = false;
+        alert('You sank my battleship!');
+      }
+    } else {
+        alert('MISS!');
+    }
+  }
+}
+
+// Вывод результата на экран
+// Точность попадания
+const shootingAccuracy = 3 / guesses * 100;
+const stats = `You took ${guesses} guesses to sink the battleship, which means your shooting accuracy was ${shootingAccuracy}%`;
+alert(stats);
